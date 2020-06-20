03fff6e9fba0919619589b69463d0acafa3e4ba4
Subash Pathak <Subash@users.noreply.github.com>
2020-06-20 21:44:37 +0545

    Update chromium.js

--- browsers/chromium.js
+++ browsers/chromium.js
@@ -1,4 +1,4 @@
-// Chromium doesn't seem to provide a simple installer for Windows and Linux
+// Chromium doesn't seem to provide a straightforward first party installer for Windows and Linux
 module.exports = {
   darwin: ['/Applications/Chromium.app/Contents/MacOS/Chromium']
 };
